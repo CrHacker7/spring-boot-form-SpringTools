039b49f3753e317f7024c3e265d77ccbc8d3eb71
CrHacker7 <crhacker7@gmail.com>
2026-07-17 18:40:41 +0200

    Populating with getters and setters and clean code

--- src/main/java/com/divdev/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/divdev/springboot/form/app/controllers/FormController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.divdev.springboot.form.app.models.domain.User;
 
@@ -18,21 +17,11 @@ public class FormController {
 	}
 
 	@PostMapping("/form")
-	public String postForm(Model model, 
-			@RequestParam String username, 
-			@RequestParam String password,
-			@RequestParam String email) {
-		
-		User user = new User();
-		user.setUsername(username);
-		user.setPassword(password);
-		user.setEmail(email);
-
-		
+	public String postForm(User user, Model model) {
+				
 		model.addAttribute("title", "Form Result");
-		model.addAttribute("username", username);
-		model.addAttribute("password", password);
-		model.addAttribute("email", email);
+		model.addAttribute("user", user);
+		
 		return "result";
 	}
 
