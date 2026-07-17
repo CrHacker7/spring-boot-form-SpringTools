d4abcc50214b64342e5f52aa73361c74aa2945fc
CrHacker7 <crhacker7@gmail.com>
2026-07-17 18:38:30 +0200

    Creating POJO User

--- src/main/java/com/divdev/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/divdev/springboot/form/app/controllers/FormController.java
@@ -4,20 +4,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.divdev.springboot.form.app.models.domain.User;
 
 @Controller
 public class FormController {
 
 	@GetMapping("/form")
 	public String getForm(Model model) {
+		model.addAttribute("title", "User Form");
 		return "form";
 	}
-	
+
 	@PostMapping("/form")
-	public String postForm(Model model) {
+	public String postForm(Model model, 
+			@RequestParam String username, 
+			@RequestParam String password,
+			@RequestParam String email) {
+		
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(password);
+		user.setEmail(email);
+
+		
+		model.addAttribute("title", "Form Result");
+		model.addAttribute("username", username);
+		model.addAttribute("password", password);
+		model.addAttribute("email", email);
 		return "result";
 	}
-	
-	
+
 }
