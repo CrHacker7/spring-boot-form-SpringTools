e700bb9ed5d9ea739fab993a4732b38917a8e3ea
CrHacker7 <crhacker7@gmail.com>
2026-07-22 10:36:56 +0200

    validating with springboot and thymeleaf

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Documentation
 - If you are using Spring Boot 2.3 or later, you need to add the `spring-boot-starter-validation` dependency.
-- To check if `spring-boot-starter-web` already includes it, use Ctrl + Click on the dependency and search for `validation`. If it is present, you do not need to add anything else. 
+- To check if `spring-boot-starter-web` already includes it, use Ctrl + Click on the dependency and search for `validation`. ==If it is present, you do not need to add anything else.==
+
+> [!note] You need to add the dependency anyway to use @Valid annotation.
+
+- **`BindingResult`**: It must be declared immediately after the object being validated.
+
+#### Validations
+- To preserve form data after a validation error, use `th:value="${user.username}"`. Ensure the property name matches the Java class field exactly, maintaining lowercase formatting, and the user class in lowercase.
--- src/main/java/com/divdev/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/divdev/springboot/form/app/controllers/FormController.java
@@ -1,7 +1,13 @@
 package com.divdev.springboot.form.app.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -12,14 +18,26 @@ public class FormController {
 
 	@GetMapping("/form")
 	public String getForm(Model model) {
+		User user = new User();
 		model.addAttribute("title", "User Form");
+		model.addAttribute("user", user);
 		return "form";
 	}
 
 	@PostMapping("/form")
-	public String postForm(User user, Model model) {
-				
+	public String postForm(@Valid User user,BindingResult result, Model model) {
+		
 		model.addAttribute("title", "Form Result");
+		if(result.hasErrors()) {
+			Map<String, String> errors = new HashMap<>();
+			result.getFieldErrors().forEach(err -> {
+				errors.put(err.getField(), "Filed ".concat(err.getField().concat(" ").concat(err.getDefaultMessage())));
+			});
+			model.addAttribute("error", errors);
+			return "form";
+		}
+				
+		
 		model.addAttribute("user", user);
 		
 		return "result";
